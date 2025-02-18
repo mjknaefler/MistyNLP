c135f4a03a84544c2a694ef85d3341f5d177593c
mknaefler <mjknaefler@gmail.com>
2025-02-18 04:44:07 -0800

    Remove redundant files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+import sys
+import os
+
+# Add the mistyPy submodule to Python's path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../third_party/Python-SDK")))
+
 # First import the robot object
 from mistyPy import Robot
+
 
 if __name__ == "__main__":
     ip_address = "192.168.1.125"
     # Create an instance of a robot
     misty = Robot(ip_address)
+    print("Connection to Misty established.")
