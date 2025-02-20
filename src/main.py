e64949ef3e22179b614efb6983e79c37a98b5883
mknaefler <mjknaefler@gmail.com>
2025-02-19 17:24:48 -0800

    Add Python-SDK to path for packages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
 import sys
 import os
 
-# Add the mistyPy submodule to Python's path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../third_party/Python-SDK")))
+# Get absolute path to MistyPy submodule
+misty_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Python-SDK/mistyPy"))
+if misty_path not in sys.path:
+    sys.path.insert(0, misty_path)
 
-# First import the robot object
-from mistyPy import Robot
+# First import the RobotGenerator
+from mistyPy.GenerateRobot import RobotGenerator
 
+# Creating a new robot generator will rewrite the RobotCommands.py and Websocket.py 
+# files to adjust them to the commands and websockets the robot has available
+RobotGenerator("192.168.1.125")
+
+# Then import the robot object
+from mistyPy.Robot import Robot
 
 if __name__ == "__main__":
     ip_address = "192.168.1.125"
